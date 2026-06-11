a16a2049fb7f32a016967ed931960c77960f3d4b
MIAOYA-AI <514609230@qq.com>
2026-06-11 12:04:06 +0800

    补充提交

---
+++ Scripts/dash.gd
@@ -0,0 +1,18 @@
+extends StateBase
+
+@export var player:Player
+
+func enter() -> void:
+	super.enter()
+	print("冲刺")
+	
+func exit() -> void:
+	super.exit()
+	if player.Direction==Vector3.ZERO:
+		state_machine.change_state("Idle")
+	else:
+		state_machine.change_state("Run")
+	
+func _on_blend_animation_tree_animation_finished(anim_name: StringName) -> void:
+	if anim_name=="Dodge_Forward":
+		exit()
