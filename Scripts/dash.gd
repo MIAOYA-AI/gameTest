0edf93b57a41e9494426dc33f9dcdab83552f105
MIAOYA-AI <514609230@qq.com>
2026-06-11 17:25:48 +0800

    添加冲刺的时间窗口计时，后续在时间窗口内可添加伤害免疫效果

--- Scripts/dash.gd
+++ Scripts/dash.gd
@@ -3,8 +3,12 @@ class_name Dash
 @export var player:Player
 @onready var timer: Timer = $Timer
 
+var dath_duration=0.2
+var time_remaining
+
 func enter() -> void:
 	timer.start()
+	time_remaining=dath_duration
 	super.enter()
 	print("冲刺")
 	
@@ -14,6 +18,11 @@ func exit() -> void:
 func physics_process_update(delta: float) -> void:
 	if player.IsDash==false:
 		state_machine.change_state("Idle")
+	time_remaining-=delta
+	if time_remaining <=0:
+		print("No dashing")
+	else:
+		print("dashing")
 	
 func _on_blend_animation_tree_animation_finished(anim_name: StringName) -> void:
 	if anim_name=="Dodge_Forward":
