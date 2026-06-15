9a8dde3af3de21fb5bfc5f3031948882e3c4792c
MIAOYA-AI <514609230@qq.com>
2026-06-15 09:07:15 +0800

    优化玩家属性更新

--- Scripts/inventory.gd
+++ Scripts/inventory.gd
@@ -5,11 +5,12 @@ class_name inventory
 @onready var endurance_value: Label = %EnduranceValue
 @onready var agility_value: Label = %AgilityValue
 @onready var level_label: Label = %LevelLabel
+@onready var player: Player =get_parent().get_owner()
 
 
-func updata_attribute(player_stats:CharacterStats) -> void:
-	level_label.text="Level "+str(player_stats.Level)
-	strength_value.text="%.2f" % player_stats.Strength.GetValue()
-	speed_value.text="%.2f" % player_stats.Speed.GetValue()
-	endurance_value.text="%.2f" % player_stats.Endurance.GetValue()
-	agility_value.text="%.2f" % player_stats.Agility.GetValue()
+func updata_attribute() -> void:
+	level_label.text="Level "+str(player.MyStats.Level)
+	strength_value.text="%.2f" % player.MyStats.Strength.GetValue()
+	speed_value.text="%.2f" % player.MyStats.Speed.GetValue()
+	endurance_value.text="%.2f" % player.MyStats.Endurance.GetValue()
+	agility_value.text="%.2f" % player.MyStats.Agility.GetValue()
--- UserInterface/user_interface.gd
+++ UserInterface/user_interface.gd
@@ -17,7 +17,7 @@ func _ready() -> void:
 		health_bar.value = player.MyStats.CurHealth
 		_updata_health_num()
 		health_component.connect("health_change", Callable(self, "_on_health_change"))
-		inventory.updata_attribute(player.MyStats)
+		inventory.updata_attribute()
 		
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("menu"):
@@ -39,7 +39,7 @@ func _on_level_up() -> void:
 		level_label.text=str(player.MyStats.Level)
 		xp_bar.max_value=player.MyStats.PercentageLevelUpBoundary()
 		xp_bar.value=player.MyStats.Xp
-		inventory.updata_attribute(player.MyStats)
+		inventory.updata_attribute()
 
 func _on_xp_up() -> void:
 	if player as Player:
