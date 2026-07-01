e60fef5dedb3f799f765bc5a3ed41484727c5a6e
MIAOYA-AI <514609230@qq.com>
2026-07-01 12:55:46 +0800

    添加视角移动说明注释

--- Scripts/CameraController.cs
+++ Scripts/CameraController.cs
@@ -22,6 +22,7 @@ public partial class CameraController : SpringArm3D
 
 		if (@event is InputEventMouseMotion mouseMotion)
 		{
+			//三维视角y轴水平视角的旋转分量由鼠标的x进行控制 x轴视角即上下视角由鼠标的y控制 z轴视角即翻转角一般不使用
 			var mouseDelta=mouseMotion.Relative;
 			Rotation = Rotation with { Y = Rotation.Y - mouseDelta.X * MouseSensitivity ,X =Mathf.Clamp(Rotation.X - mouseDelta.Y * MouseSensitivity,-Single.Pi/2,Single.Pi/4)};
 		}
